ea227c5f78afcdc7025d7b31f87e1ebd6d425521
bum3144 <bum3144@gmail.com>
2019-12-12 23:07:46 +0800

    numberBaseball

--- 03.numberBaseball/webpack.config.js
+++ 03.numberBaseball/webpack.config.js
@@ -1,12 +1,26 @@
-module.exports = {
-  entry: {
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
 
+module.exports = {
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  entry: {
+    app: path.join(__dirname, 'main.js'),
   },
   module: {
-
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+    }],
   },
-  plugins: [ ],
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
   output: {
-
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
   },
-}
+};
